cbfbd452b326fa1316a3f3296b00544a7e306bce
Wilson <wilson_l@foxmail.com>
2022-04-11 21:18:52 +0800

    delete some unnecessary files

--- src/components/signIn/api/api.js
+++ src/components/signIn/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import baseURL from '@/assets/js/baseURL_config'
 
 // params: user:{name:'', password:'',}
-// return: resp:{status:'', msg:'', uid:''}
+// return: res:{status:'', msg:'', uid:''}
 export function login(data) {
     return axios({
       url: baseURL + '/Users/login',
